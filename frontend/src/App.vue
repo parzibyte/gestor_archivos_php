1914c726a968953a7dfbcd32ee8fa70922df86e8
Luis Cabrera Benito <parzibyte@gmail.com>
2021-01-22 23:55:43 -0600

    Agregando esqueleto del frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="container">
+      <div class="columns">
+        <div class="column">
+          <section>
+            <h1 class="is-size-1">Probando Buefy</h1>
+            <button class="button is-medium" @click="mostrarToast">
+                Mostrar un toast
+            </button>
+          </section>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  methods: {
+    mostrarToast(){
+      this.$buefy.toast.open("¡Hola, Buefy! Parzibyte.me")
+    }
+  }
+}
+</script>
