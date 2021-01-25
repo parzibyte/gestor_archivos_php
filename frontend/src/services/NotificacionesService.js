1f65b183ce6cd33f908ca9fa823eb18ec212c068
Luis Cabrera Benito <parzibyte@gmail.com>
2021-01-25 14:33:19 -0600

    Agregando método para mostrar diálogo de confirmación

--- frontend/src/services/NotificacionesService.js
+++ frontend/src/services/NotificacionesService.js
@@ -1,4 +1,5 @@
 import {NotificationProgrammatic as Notification} from 'buefy'
+import {DialogProgrammatic as Dialog} from 'buefy'
 
 const NotificacionesService = {
     mostrarNotificacionExito(mensaje) {
@@ -13,6 +14,18 @@ const NotificacionesService = {
             message: mensaje,
             type: 'is-danger',
             position: "is-top-left"
+        });
+    },
+    mostrarDialogoConfirmacion(pregunta, callback, mensajeOk, mensajeCancelar) {
+        mensajeOk = mensajeOk || "Ok";
+        mensajeCancelar = mensajeCancelar || "Cancelar";
+        Dialog.confirm({
+            title: 'Confirmar',
+            message: pregunta,
+            cancelText: mensajeCancelar,
+            confirmText: mensajeOk,
+            type: 'is-danger',
+            onConfirm: callback,
         });
     }
 };
