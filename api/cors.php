3934884eb646dad88991f014b7f97757503bc9fe
Luis Cabrera Benito <parzibyte@gmail.com>
2021-01-26 13:50:35 -0600

    CORS ahora permite cualquier dominio

--- api/cors.php
+++ api/cors.php
@@ -1,5 +1,5 @@
 <?php
-$dominioPermitido = "http://localhost:8080";
+$dominioPermitido = $_SERVER['HTTP_ORIGIN'];
 header("Access-Control-Allow-Origin: $dominioPermitido");
 header("Access-Control-Allow-Headers: content-type");
 header("Access-Control-Allow-Credentials: true");
